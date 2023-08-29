83994452f7db8b1a0a74f3dcc46e8044342627ee
Mr3K13 <ph154522@gmail.com>
2023-08-28 21:55:51 -0300

    execicio-2

--- Exercicios de fixação/um/umf.java
+++ Exercicios de fixação/um/umf.java
@@ -6,10 +6,10 @@ public class umf {
   public static void main(String[] args) {
     um x = new um();
     Scanner sc = new Scanner(System.in);
-    System.out.print("Qual o primeiro valor da cateto? ");
+    System.out.print("Qual o primeiro valor do cateto? ");
     x.a = (sc.nextInt());
 
-    System.out.print("Qual o segundo valor da cateto? ");
+    System.out.print("Qual o segundo valor do cateto? ");
     Scanner sx = new Scanner(System.in);
     x.b = (sx.nextInt());
     x.Hypotenusa();
