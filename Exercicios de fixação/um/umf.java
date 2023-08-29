ffdcf2068181a998783946125750652160002bd0
Mr3K13 <ph154522@gmail.com>
2023-08-28 21:40:21 -0300

    exercicio-2

--- Exercicios de fixação/um/umf.java
+++ Exercicios de fixação/um/umf.java
@@ -6,10 +6,10 @@ public class umf {
   public static void main(String[] args) {
     um x = new um();
     Scanner sc = new Scanner(System.in);
-    System.out.print("Qual o primeiro valor da hipotenusa? ");
+    System.out.print("Qual o primeiro valor da cateto? ");
     x.a = (sc.nextInt());
 
-    System.out.print("Qual o segundo valor da hipotenusa? ");
+    System.out.print("Qual o segundo valor da cateto? ");
     Scanner sx = new Scanner(System.in);
     x.b = (sx.nextInt());
     x.Hypotenusa();
