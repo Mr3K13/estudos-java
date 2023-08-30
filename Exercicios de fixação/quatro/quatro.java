7f8a3349d15ad91e6ee1285346cc113dc6aac521
Mr3K13 <ph154522@gmail.com>
2023-08-30 17:27:47 -0300

    att1.3

--- Exercicios de fixação/quatro/quatro.java
+++ Exercicios de fixação/quatro/quatro.java
@@ -6,15 +6,15 @@ public class quatro {
   public void resultado() {
     if (valor1 > valor2) {
       if (valor1 > valor3) {
-        System.out.print("O primeiro valor(" + valor1 + ") é o maior.");
+        System.out.print("O primeiro valor (" + valor1 + ") é o maior.");
 
       } else {
-        System.out.print("O terceiro valor(" + valor3 + ") é o maior.");
+        System.out.print("O terceiro valor (" + valor3 + ") é o maior.");
       }
     } else if (valor2 > valor3) {
-      System.out.print("O segundo valor(" + valor2 + ") é o maior.");
+      System.out.print("O segundo valor (" + valor2 + ") é o maior.");
     } else {
-      System.out.print("O terceiro valor(" + valor3 + ") é o maior.");
+      System.out.print("O terceiro valor (" + valor3 + ") é o maior.");
     }
   }
 }
