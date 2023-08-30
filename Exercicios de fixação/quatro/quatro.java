26b838cf970d13069478ebd93dc392a789049512
Mr3K13 <ph154522@gmail.com>
2023-08-30 17:18:52 -0300

    att 1.1

--- Exercicios de fixação/quatro/quatro.java
+++ Exercicios de fixação/quatro/quatro.java
@@ -3,7 +3,17 @@ package quatro;
 public class quatro {
   public double valor1, valor2, valor3;
 
-  public void verificador() {
-  }
+  public void resultado() {
+    if (valor1 > valor2) {
+      if (valor1 > valor3) {
+        System.out.print("O primeiro valor(" + valor1 + ") é o maior.");
 
+      }
+      System.out.print("O terceiro valor(" + valor3 + ") é o maior.");
+    } else if (valor2 > valor3) {
+      System.out.print("O segundo valor(" + valor2 + ") é o maior.");
+    } else {
+      System.out.print("O terceiro valor(" + valor3 + ") é o maior.");
+    }
+  }
 }
--- Exercicios de fixação/quatro/quatrom.java
+++ Exercicios de fixação/quatro/quatrom.java
@@ -15,17 +15,6 @@ public class quatrom {
     System.out.print("Qual o terceiro valor? ");
     x.valor3 = (sc.nextInt());
     sc.close();
-    x.verificador();
-    if (x.valor1 > x.valor2) {
-      if (x.valor1 > x.valor3) {
-        System.out.print("O primeiro valor(" + x.valor1 + ") é o maior.");
-
-      }
-      System.out.print("O terceiro valor(" + x.valor3 + ") é o maior.");
-    } else if (x.valor2 > x.valor3) {
-      System.out.print("O segundo valor(" + x.valor2 + ") é o maior.");
-    } else {
-      System.out.print("O terceiro valor(" + x.valor3 + ") é o maior.");
-    }
+    x.resultado();
   }
 }
